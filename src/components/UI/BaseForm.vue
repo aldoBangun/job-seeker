981b9425c356b1911ffc592be0c5e38b3000809c
aldoBangun <punk.aldo68@gmail.com>
2020-11-02 17:21:01 +0800

    add BaseForm

---
+++ src/components/UI/BaseForm.vue
@@ -0,0 +1,27 @@
+<template>
+   <form :class="mode">
+      <slot><slot>
+   </form>   
+</template>
+
+<script>
+export default {
+   props: {
+      mode: {
+         type: String,
+         required: false
+      }
+   }
+}
+</script>
+
+<style scoped>
+.form-group {
+   position: relative;
+}
+
+input {
+   display: block;
+   width: 100%;
+}
+</style>
