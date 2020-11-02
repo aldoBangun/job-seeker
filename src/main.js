39050408b530a9cc61c0ce8f0fdc1c3bc3fabd3e
aldoBangun <punk.aldo68@gmail.com>
2020-11-02 17:22:08 +0800

    add BaseForm to main.js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from './store/';
 import App from './App.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 import BaseContainer from './components/UI/BaseContainer.vue';
+import BaseForm from './components/UI/BaseForm.vue';
 
 const app = createApp(App);
 
 app.component('base-button', BaseButton);
 app.component('base-container', BaseContainer);
+app.component('base-form', BaseForm);
 
 app.use(store);
 app.use(router);
