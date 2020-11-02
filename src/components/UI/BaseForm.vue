5ca1d117c2d7b4577bd27b62882298ee020faa5d
aldoBangun <punk.aldo68@gmail.com>
2020-11-02 19:22:18 +0800

    update BaseForm

--- src/components/UI/BaseForm.vue
+++ src/components/UI/BaseForm.vue
@@ -1,27 +1,31 @@
 <template>
-   <form :class="mode">
-      <slot><slot>
-   </form>   
+  <form :class="mode">
+    <slot></slot>
+  </form>
 </template>
 
 <script>
 export default {
-   props: {
-      mode: {
-         type: String,
-         required: false
-      }
-   }
-}
+  props: {
+    mode: {
+      type: String,
+      required: false
+    }
+  }
+};
 </script>
 
 <style scoped>
+form > * + * {
+  margin: 2rem 0 0;
+}
+
 .form-group {
-   position: relative;
+  position: relative;
 }
 
 input {
-   display: block;
-   width: 100%;
+  display: block;
+  width: 100%;
 }
 </style>
