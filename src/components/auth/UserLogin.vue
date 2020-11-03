df7f8d6066245d0320549893817cb4b9ec5e8ce0
aldoBangun <punk.aldo68@gmail.com>
2020-11-03 08:50:29 +0800

    add user login functionality at UserLogin.vue, store/actions.js

--- src/components/auth/UserLogin.vue
+++ src/components/auth/UserLogin.vue
@@ -1,29 +1,32 @@
 <template>
-  <base-card>
-    <base-form @submit.prevent="login">
-      <div class="form-group" :class="errorEmail">
-        <label for="email">email</label>
-        <input type="text" id="email" v-model.trim="email" />
-        <small class="error" v-if="invalidEmail">
-          Please enter a valid email
-        </small>
-      </div>
-      <div class="form-group" :class="errorPassword">
-        <label for="password">password</label>
-        <input type="password" id="password" v-model.trim="password" />
-        <small class="error" v-if="invalidPassword">
-          Password must have at least 8 characters
-        </small>
-      </div>
-      <base-button mode="btn-primary btn-block btn-rounded">
-        Sign In
-      </base-button>
-      <p>
-        Not have any account yet?
-        <router-link to="register"> Sign Up </router-link>here!
-      </p>
-    </base-form>
-  </base-card>
+  <div>
+    <p v-if="error">{{ error }}</p>
+    <base-card>
+      <base-form @submit.prevent="login">
+        <div class="form-group" :class="errorEmail">
+          <label for="email">email</label>
+          <input type="text" id="email" v-model.trim="email" />
+          <small class="error" v-if="invalidEmail && !error">
+            Please enter a valid email
+          </small>
+        </div>
+        <div class="form-group" :class="errorPassword">
+          <label for="password">password</label>
+          <input type="password" id="password" v-model.trim="password" />
+          <small class="error" v-if="invalidPassword && !error">
+            Password must have at least 8 characters
+          </small>
+        </div>
+        <base-button mode="btn-primary btn-block btn-rounded">
+          Sign In
+        </base-button>
+        <p>
+          Not have any account yet?
+          <router-link to="register"> Sign Up </router-link>here!
+        </p>
+      </base-form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -33,7 +36,8 @@ export default {
       email: '',
       password: '',
       invalidEmail: false,
-      invalidPassword: false
+      invalidPassword: false,
+      error: null
     };
   },
   computed: {
@@ -45,9 +49,10 @@ export default {
     }
   },
   methods: {
-    login() {
+    async login() {
       this.invalidEmail = false;
       this.invalidPassword = false;
+      this.error = null;
 
       const { email, password } = this;
 
@@ -60,9 +65,18 @@ export default {
 
       const user = { email, password };
 
-      this.email = '';
-      this.password = '';
-      console.log(user);
+      try {
+        await this.$store.dispatch('login', user);
+
+        this.email = '';
+        this.password = '';
+
+        this.$router.replace('/jobs');
+      } catch (error) {
+        this.error = error;
+        this.invalidEmail = true;
+        this.invalidPassword = true;
+      }
     }
   }
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,10 @@
 export default {
-  register(context, payload) {
+  getUsers() {
     const users = JSON.parse(localStorage.getItem('users')) || [];
+    return users;
+  },
+  register(context, payload) {
+    const users = context.dispatch('getUsers');
     const id = `u${users.length + 1}`;
     const existUser = users.find(user => user.email === payload.email);
 
@@ -18,5 +22,20 @@ export default {
     localStorage.setItem('users', JSON.stringify(users));
 
     context.commit('setActiveUser', payload);
+  },
+  async login(context, payload) {
+    const users = await context.dispatch('getUsers');
+    const { email, password } = payload;
+    const validUser = users.find(user => {
+      return user.email === email && user.password === password;
+    });
+
+    if (!validUser) {
+      throw new Error(
+        'Failed to signing in. please check your email or password'
+      );
+    }
+
+    context.commit('setActiveUser', validUser);
   }
 };
