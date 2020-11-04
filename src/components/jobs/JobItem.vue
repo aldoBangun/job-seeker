8e59dcc8983bfd23f56d82ccbae5c70c5d98f8e4
aldoBangun <punk.aldo68@gmail.com>
2020-11-04 14:29:36 +0800

    add JobItem.vue

---
+++ src/components/jobs/JobItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <base-card mode="card-flat" :title="job.title">
+    <h3>Applied : {{ job.proposals.length }} times</h3>
+    <h3>Skill Requirements:</h3>
+    <div class="cat">
+      <base-badge v-for="cat in job.categories" :key="cat" :title="cat">
+      </base-badge>
+    </div>
+    <div class="actions">
+      <base-button mode="btn-outline">Details</base-button>
+      <base-button mode="btn-primary" v-if="job.userId !== userId">
+        Apply Now
+      </base-button>
+    </div>
+  </base-card>
+</template>
+
+<script>
+export default {
+  props: {
+    job: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    userId() {
+      return this.$store.getters.user.userId;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.cat,
+.actions {
+  display: flex;
+  align-items: center;
+  gap: 0.25em;
+}
+
+.actions {
+  /* justify-content: flex-end; */
+  gap: 1em;
+  margin: 2rem 0 0;
+}
+</style>
