9fb159b896b32e85037f0272e9b71ba5ef7d5a79
aldoBangun <punk.aldo68@gmail.com>
2020-11-06 10:14:29 +0800

    add form validation to AddJob.vue

--- src/components/user/AddJob.vue
+++ src/components/user/AddJob.vue
@@ -1,21 +1,30 @@
 <template>
   <base-card>
     <base-form @submit.prevent="addJob">
-      <div class="form-group">
+      <div class="form-group" :class="invalidInput('title')">
         <label for="title">title</label>
         <input type="text" id="title" v-model.trim="title" />
+        <small class="error" v-if="invalidTitle"
+          >Title Shouldn't be empty</small
+        >
       </div>
-      <div class="form-group">
+      <div class="form-group" :class="invalidInput('cat')">
         <label for="categories">categories</label>
         <input type="text" id="categories" v-model.trim="categories" />
+        <small class="error" v-if="invalidCat"
+          >Please input at lease 1 categories</small
+        >
       </div>
-      <div class="form-group">
+      <div class="form-group" :class="invalidInput('desc')">
         <label for="description">description</label>
         <textarea
           id="description"
           v-model.trim="description"
           rows="4"
         ></textarea>
+        <small class="error" v-if="invalidDesc"
+          >Description shouldn't be empty</small
+        >
       </div>
 
       <base-button mode="btn-block btn-primary btn-rounded">
@@ -31,25 +40,44 @@ export default {
     return {
       title: '',
       description: '',
-      categories: ''
+      categories: '',
+      invalidTitle: false,
+      invalidDesc: false,
+      invalidCat: false
     };
   },
   methods: {
+    invalidInput(input) {
+      if (input === 'title') return { error: this.invalidTitle };
+      if (input === 'cat') return { error: this.invalidCat };
+      if (input === 'desc') return { error: this.invalidDesc };
+    },
     addJob() {
+      this.invalidTitle = false;
+      this.invalidDesc = false;
+      this.invalidCat = false;
+
       const { title, description, categories } = this;
-      const { userId } = this.$store.getters.user;
-      const newJob = {
-        userId,
-        title,
-        description,
-        categories: categories.split(', ')
-      };
 
-      this.$store.dispatch('jobs/addJob', newJob);
+      if (title !== '' && description !== '' && categories !== '') {
+        const { userId } = this.$store.getters.user;
+        const newJob = {
+          userId,
+          title,
+          description,
+          categories: categories.split(', ')
+        };
 
-      this.title = '';
-      this.categories = '';
-      this.description = '';
+        this.$store.dispatch('jobs/addJob', newJob);
+
+        this.title = '';
+        this.categories = '';
+        this.description = '';
+      } else {
+        if (title === '') this.invalidTitle = true;
+        if (description === '') this.invalidDesc = true;
+        if (categories === '') this.invalidCat = true;
+      }
     }
   }
 };
