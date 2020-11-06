a3489024509ce21478a972c24f2702ddf1c04008
aldoBangun <punk.aldo68@gmail.com>
2020-11-06 12:49:57 +0800

    add request counter to UserJobs.vue

--- src/components/user/UserJobs.vue
+++ src/components/user/UserJobs.vue
@@ -2,9 +2,13 @@
   <base-card mode="card-flat" title="Jobs Info">
     <h3>Job Posted</h3>
     <p>{{ jobs.length }} Job(s)</p>
+
     <h3>Last Job</h3>
     <p v-if="jobs.length">{{ jobs[jobs.length - 1].title }}</p>
     <p v-else>No Jobs posted yet</p>
+
+    <h3>Requests</h3>
+    <p>{{ proposals }} interview requests</p>
   </base-card>
 </template>
 
@@ -16,7 +20,16 @@ export default {
     },
     jobs() {
       const jobs = this.$store.getters['jobs/jobs'];
+
       return jobs.filter(job => job.userId === this.userId) || [];
+    },
+    proposals() {
+      const { jobs } = this;
+      const proposals = jobs
+        .map(job => job.proposals.length)
+        .reduce((total, prop) => total + prop);
+
+      return proposals;
     }
   }
 };
